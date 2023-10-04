b8b0cd2a0391d2bb5b016dbf5718ee98ac3c8c4e
dchristman <dchristman@indeed.com>
2023-10-04 07:33:43 -0500

    Adding wait for db functions to yml and docker-compose

--- app/app/tests.py
+++ app/app/tests.py
@@ -3,7 +3,7 @@ Sample tests
 """
 from django.test import SimpleTestCase
 
-from app import calc 
+from app import calc
 
 
 class CalcTests(SimpleTestCase):
@@ -12,7 +12,7 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-    
+
     def test_subtract_numbers(self):
         res = calc.subtract(10, 15)
 
