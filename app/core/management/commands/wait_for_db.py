9288242e85b6f6c5ed95fee7ec1c15577e224366
dchristman <dchristman@indeed.com>
2023-10-04 06:11:08 -0500

    Added partial db connections

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,12 @@
+"""
+Django command to wait for the database to be available
+"""
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    """
+    Django Command to Wait for Database
+    """
+
+    def handle(self, *args, **options):
+        pass
